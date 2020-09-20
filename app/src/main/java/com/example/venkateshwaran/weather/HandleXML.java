bd9b6af8441b9cf3bd0d678d8596631c69d709f2
Venkateshwaran97 <venkateshwaran.1997@gmail.com>
2020-09-20 18:31:45 +0530

    Reformat and refactor code

--- app/src/main/java/com/example/venkateshwaran/weather/HandleXML.java
+++ app/src/main/java/com/example/venkateshwaran/weather/HandleXML.java
@@ -2,6 +2,7 @@ package com.example.venkateshwaran.weather;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -16,102 +17,81 @@ public class HandleXML {
     private XmlPullParserFactory xmlFactoryObject;
     public volatile boolean parsingComplete = true;
 
-    public HandleXML(String url){
+    public HandleXML(String url) {
         this.urlString = url;
     }
 
-    public String getCountry(){
+    public String getCountry() {
         return country;
     }
 
-    public String getTemperature(){
+    public String getTemperature() {
         return temperature;
     }
 
-    public String getHumidity(){
+    public String getHumidity() {
         return humidity;
     }
 
-    public String getPressure(){
+    public String getPressure() {
         return pressure;
     }
 
     public void parseXMLAndStoreIt(XmlPullParser myParser) {
         int event;
-        String text=null;
-
+        String text = null;
         try {
             event = myParser.getEventType();
-
             while (event != XmlPullParser.END_DOCUMENT) {
-                String name=myParser.getName();
-
-                switch (event){
+                String name = myParser.getName();
+                switch (event) {
                     case XmlPullParser.START_TAG:
-                         if(name.equals("humidity")){
-                        humidity = myParser.getAttributeValue(null,"value");
-                    }
-
-                    else if(name.equals("pressure")){
-                        pressure = myParser.getAttributeValue(null,"value");
-                    }
-
-                     if(name.equals("temperature")){
-                        temperature = myParser.getAttributeValue(null,"value");
-                         
-                    }
-                        if(name.equals("weather")){
-                            country = myParser.getAttributeValue(null,"value");;
+                        if (name.equals("humidity")) {
+                            humidity = myParser.getAttributeValue(null, "value");
+                        } else if (name.equals("pressure")) {
+                            pressure = myParser.getAttributeValue(null, "value");
+                        }
+                        if (name.equals("temperature")) {
+                            temperature = myParser.getAttributeValue(null, "value");
+                        }
+                        if (name.equals("weather")) {
+                            country = myParser.getAttributeValue(null, "value");
                         }
                         break;
-
                     case XmlPullParser.TEXT:
                         text = myParser.getText();
                         break;
-
                     case XmlPullParser.END_TAG:
-
-
-
-
-
                         break;
                 }
                 event = myParser.next();
             }
             parsingComplete = false;
-        }
-
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void fetchXML(){
-        Thread thread = new Thread(new Runnable(){
+    public void fetchXML() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     URL url = new URL(urlString);
-                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setReadTimeout(10000 /* milliseconds */);
                     conn.setConnectTimeout(15000 /* milliseconds */);
                     conn.setRequestMethod("GET");
                     conn.setDoInput(true);
                     conn.connect();
-
                     InputStream stream = conn.getInputStream();
                     xmlFactoryObject = XmlPullParserFactory.newInstance();
                     XmlPullParser myparser = xmlFactoryObject.newPullParser();
-
                     myparser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
                     myparser.setInput(stream, null);
-
                     parseXMLAndStoreIt(myparser);
                     stream.close();
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
--- app/src/main/java/com/example/venkateshwaran/weather/MainActivity.java
+++ app/src/main/java/com/example/venkateshwaran/weather/MainActivity.java
@@ -9,6 +9,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,13 +22,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     EditText ed1;
-    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String MyPREFERENCES = "MyPrefs";
     public static final String Name = "nameKey";
-
-TextView ed2,ed3,ed4,ed5;
-
+    TextView ed2, ed3, ed4, ed5;
     private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
     private String url2 = "&mode=xml&APPID=388fdb71c09bd0d16cd8e3972111cc6f";
     private HandleXML obj;
@@ -36,39 +35,31 @@ TextView ed2,ed3,ed4,ed5;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=(Button)findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.button);
+        ed1 = (EditText) findViewById(R.id.editText);
+        ed2 = (TextView) findViewById(R.id.editText2);
+        ed3 = (TextView) findViewById(R.id.textView);
+        ed4 = (TextView) findViewById(R.id.editText4);
+        ed5 = (TextView) findViewById(R.id.editText5);
 
-        ed1=(EditText)findViewById(R.id.editText);
-        ed2=(TextView)findViewById(R.id.editText2);
-        ed3=(TextView)findViewById(R.id.textView);
-        ed4=(TextView)findViewById(R.id.editText4);
-        ed5=(TextView)findViewById(R.id.editText5);
-        ed1.setOnKeyListener(new View.OnKeyListener()
-        {
+        ed1.setOnKeyListener(new View.OnKeyListener() {
 
-            public boolean onKey(View v, int keyCode, KeyEvent event)
-            {
-                if (event.getAction() == KeyEvent.ACTION_DOWN)
-                {
-                    switch (keyCode)
-                    {
-
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
                         case KeyEvent.KEYCODE_ENTER:
                             String url = ed1.getText().toString();
                             String finalUrl = url1 + url + url2;
                             ed2.setText(finalUrl);
                             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                             imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-
                             obj = new HandleXML(finalUrl);
                             obj.fetchXML();
-
-                            while(obj.parsingComplete);
-                            ed2.setText("Sky:"+obj.getCountry());
-                            ed3.setText((String.valueOf(String.format("%.1f",Float.parseFloat(obj.getTemperature())-273)))+" ℃");
-                            ed4.setText("Humidity:"+obj.getHumidity()+"%");
-                            ed5.setText("Pressure:"+obj.getPressure()+"hPa");
-
+                            while (obj.parsingComplete) ;
+                            ed2.setText("Sky:" + obj.getCountry());
+                            ed3.setText((String.valueOf(String.format("%.1f", Float.parseFloat(obj.getTemperature()) - 273))) + " ℃");
+                            ed4.setText("Humidity:" + obj.getHumidity() + "%");
+                            ed5.setText("Pressure:" + obj.getPressure() + "hPa");
                             return true;
                         default:
                             break;
@@ -79,7 +70,6 @@ TextView ed2,ed3,ed4,ed5;
         });
 
 
-
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,10 +78,8 @@ TextView ed2,ed3,ed4,ed5;
                 ed2.setText(finalUrl);
                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-
                 obj = new HandleXML(finalUrl);
                 obj.fetchXML();
-
                 while (obj.parsingComplete) ;
                 ed2.setText("Sky:" + obj.getCountry());
                 ed3.setText((String.valueOf(String.format("%.1f", Float.parseFloat(obj.getTemperature()) - 273))) + " C");
@@ -100,14 +88,12 @@ TextView ed2,ed3,ed4,ed5;
             }
         });
 
-        SharedPreferences sharedpreferences= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
-
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedpreferences.edit();
-        editor.putString(Name,ed1.getText().toString());
+        editor.putString(Name, ed1.getText().toString());
         editor.commit();
-      Toast toast=Toast.makeText(getApplicationContext(), sharedpreferences.getString(Name,"Chennai"),Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(), sharedpreferences.getString(Name, "Chennai"), Toast.LENGTH_SHORT);
         toast.show();
     }
 
@@ -123,9 +109,7 @@ TextView ed2,ed3,ed4,ed5;
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
